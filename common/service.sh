e905143464fe7c920d54f9f8b10c2c32dd08abbf
Adrian Miller <adrian.m.miller@gmail.com>
2019-06-30 03:34:36 +1000

    Update service.sh
    - Added extra chattr path possiblity 
    - Added sleep between commands

--- common/service.sh
+++ common/service.sh
@@ -22,17 +22,25 @@ fi
 if [ -f /data/data/com.termux/files/usr/bin/applets/chattr ]
 then
 chattrpath=/data/data/com.termux/files/usr/bin/applets
-else
-chattrpath=/system/bin
+elif [ -f /system/bin ] 
+then 
+  chattrpath=/system/bin
+elif [ -f /system/xbin ] 
+then 
+  chattrpath=/system/xbin
 fi
 
 # On to the main show the SQL commands and database permissions
 /system/bin/am force-stop /data/data/com.google.android.apps.walletnfcrel
+sleep 2
 $chattrpath/chattr -i /data/data/com.google.android.gms/databases/dg.db
+sleep 2
 /system/bin/chmod 777 /data/data/com.google.android.gms/databases/dg.db
+sleep 2
 $sqlpath/sqlite3 /data/data/com.google.android.gms/databases/dg.db "update main set c='0' where a like '%attest%';"
+sleep 2
 /system/bin/chmod 440 /data/data/com.google.android.gms/databases/dg.db
+sleep 2
 $chattrpath/chattr +i /data/data/com.google.android.gms/databases/dg.db
-
+sleep 2
 exit
-

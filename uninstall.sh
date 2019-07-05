f84f0589594875b8caa203a5b768e5f339be2e0c
Adrian Miller <adrian.m.miller@gmail.com>
2019-07-06 07:43:49 +1000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,18 +1,3 @@
-#!/system/bin/sh
-# Do NOT assume where your module will be located.
-# ALWAYS use $MODDIR if you need to know where this script
-# and module is placed.
-# This will make sure your module will still work
-# if Magisk change its mount point in the future
-MODDIR=${0%/*}
-
-#  find sdcard
-if [ -d /sdcard ] ; then
-    sdcardpath=/sdcard
-elif [ -d /storage/emulated/0 ] ; then
-    sdcardpath=/storage/emulated/0
-fi
-
 # set payfixuninstall.log
 logfile=/cache/payfixuninstall.log
 
@@ -23,25 +8,34 @@ echo "" > $logfile
 if [ -f /data/data/com.termux/files/usr/bin/applets/chattr ] ; then
 	chattrpath=/data/data/com.termux/files/usr/bin/applets
 	echo "Chattr binary found in: $chattrpath" >> $logfile
+	echo "" >> $logfile
 elif [ -f /system/bin/chattr ] ; then 
 	chattrpath=/system/bin
 	echo "Chattr binary found in: $chattrpath" >> $logfile
+	echo "" >> $logfile
 elif [ -f /system/xbin/chattr ] ; then 
 	chattrpath=/system/xbin
   	echo "Chattr binary found in: $chattrpath" >> $logfile
+	echo "" >> $logfile
 else 
-	echo "Chattr binary not found" >> $logfile
+	echo "Chattr binary not found, BusyBox is needed to provide chattr, which is used to make the database immutable" >> $logfile
+	echo "Without chattr this fix *may* not work, though people report that the fix works fine for them without using"  >> $logfile
+	echo "chattr. However, if you want the full fix script to run as designed, please install BusyBox for Android NDK from" >> $logfile 
+	echo  "the Magisk Repo" >> $logfile
+	echo "" >> $logfile
 fi
 sleep 2
 
-# on to the main show the database permissions
+# on to the main show............
 
 # stop Google Pay
-/system/bin/am force-stop /data/data/com.google.android.apps.walletnfcrel
+am force-stop /data/data/com.google.android.apps.walletnfcrel
 if [ $? -eq 0 ] ; then
 	echo "Google Pay stopped successfully" >> $logfile
+	echo "" >> $logfile
 else
 	echo "Google Pay NOT stopped successfully" >> $logfile
+	echo "" >> $logfile
 fi
 sleep 2
 
@@ -49,35 +43,44 @@ sleep 2
 $chattrpath/chattr -i /data/data/com.google.android.gms/databases/dg.db
 if [ $? -eq 0 ] ; then
 	echo "Chattr command completed successfully" >> $logfile
+	echo "" >> $logfile
 else
 	echo "Chattr command FAILED" >> $logfile
+	echo "" >> $logfile
 fi
 sleep 2
 
 # set default 660 permissions on dg.db
-/system/bin/chmod 660 /data/data/com.google.android.gms/databases/dg.db
-if [ $? -eq 0 ] ; then
+chmod 660 /data/data/com.google.android.gms/databases/dg.db
+perms=$(stat -c %a /data/data/com.google.android.gms/databases/dg.db)
+if [ $perms -eq 660 ] ; then
 	echo "Chmod 660 command completed successfully" >> $logfile
+	echo "Permissions reported as: $perms" >> $logfile
+	echo "" >> $logfile
 else
 	echo "Chmod 660 command FAILED" >> $logfile
+	echo "Permissions reported as: $perms" >> $logfile
+	echo "" >> $logfile
 fi
 sleep 2
 
 # remove /data/adb/service.d/gpay.sh
-/system/bin/rm -rf /data/adb/service.d/gpay.sh
+rm -rf /data/adb/service.d/gpay.sh
 if [ $? -eq 0 ] ; then
 	echo "Gpay.sh script removed successfully" >> $logfile
+	echo "" >> $logfile
 else
 	echo "Gpay.sh script removal FAILED" >> $logfile
+	echo "" >> $logfile
 fi
 sleep 2
 
-# remove /sdcard/payfixfirstrun.log - this is the onetime runlog from first run of /data/adb/service/gpay.sh
-/system/bin/rm -rf $sdcardpath/payfixfirstrun.log
+# remove /cache/payfixfirstrun.log - this is the onetime runlog from first run of /data/adb/service/gpay.sh
+rm -rf /cache/payfixfirstrun.log
 if [ $? -eq 0 ] ; then
 	echo "Payfix firstrun log removed successfully" >> $logfile
+	echo "" >> $logfile
 else
 	echo "Payfix firstrun log removal FAILED" >> $logfile
+	echo "" >> $logfile
 fi
-
-exit

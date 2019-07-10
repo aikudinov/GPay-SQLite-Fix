4f8796189feff0fa7612d1ad30620ae13227daaa
Adrian Miller <adrian.m.miller@gmail.com>
2019-07-11 02:36:05 +1000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,35 +1,47 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
 # set payfixuninstall.log
 logfile=/cache/payfixuninstall.log
 
-# create logfile
-echo "" > $logfile
 
-# check where chattr is
-if [ -f /data/data/com.termux/files/usr/bin/applets/chattr ] ; then
-	chattrpath=/data/data/com.termux/files/usr/bin/applets
-	echo "Chattr binary found in: $chattrpath" >> $logfile
+# wait till boot completed  - thanks to jcmm11
+until [ "$(getprop sys.boot_completed)" = 1 ]
+do
+	sleep 1
+done
+
+echo "System boot completed" > $logfile
+echo "" >> $logfile
+
+while (true); do
+if [ -f "/system/bin/chmod" ]; then
+	echo "/system/bin/chmod....accessible" >> $logfile
 	echo "" >> $logfile
-elif [ -f /system/bin/chattr ] ; then 
-	chattrpath=/system/bin
-	echo "Chattr binary found in: $chattrpath" >> $logfile
-	echo "" >> $logfile
-elif [ -f /system/xbin/chattr ] ; then 
-	chattrpath=/system/xbin
-  	echo "Chattr binary found in: $chattrpath" >> $logfile
-	echo "" >> $logfile
-else 
-	echo "Chattr binary not found, BusyBox is needed to provide chattr, which is used to make the database immutable" >> $logfile
-	echo "Without chattr this fix *may* not work, though people report that the fix works fine for them without using"  >> $logfile
-	echo "chattr. However, if you want the full fix script to run as designed, please install BusyBox for Android NDK from" >> $logfile 
-	echo  "the Magisk Repo" >> $logfile
-	echo "" >> $logfile
+    break;
 fi
-sleep 2
+	sleep 1
+done
+
+# check the dg.db file is accessible before we start off doing anything - thanks to didgeridoohan
+while (true); do
+if [ -f "/data/data/com.google.android.gms/databases/dg.db" ]; then
+	echo "Database file dg.db....accessible" >> $logfile
+	echo "" >> $logfile
+    break;
+fi
+	sleep 1
+done
 
 # on to the main show............
 
 # stop Google Pay
-am force-stop /data/data/com.google.android.apps.walletnfcrel
+/system/bin/am force-stop /data/data/com.google.android.apps.walletnfcrel
 if [ $? -eq 0 ] ; then
 	echo "Google Pay stopped successfully" >> $logfile
 	echo "" >> $logfile
@@ -39,19 +51,8 @@ else
 fi
 sleep 2
 
-# undo dg.db immutability
-$chattrpath/chattr -i /data/data/com.google.android.gms/databases/dg.db
-if [ $? -eq 0 ] ; then
-	echo "Chattr command completed successfully" >> $logfile
-	echo "" >> $logfile
-else
-	echo "Chattr command FAILED" >> $logfile
-	echo "" >> $logfile
-fi
-sleep 2
-
 # set default 660 permissions on dg.db
-chmod 660 /data/data/com.google.android.gms/databases/dg.db
+/system/bin/chmod 660 /data/data/com.google.android.gms/databases/dg.db
 perms=$(stat -c %a /data/data/com.google.android.gms/databases/dg.db)
 if [ $perms -eq 660 ] ; then
 	echo "Chmod 660 command completed successfully" >> $logfile
